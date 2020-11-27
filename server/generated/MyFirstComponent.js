8d43b6cb9874fa76c3f18c283d48ae2c40547fc7
gautamprasad16 <prasadgautam16@gmail.com>
2020-11-27 12:51:45 +0530

    Handlebars integration

--- server/generated/MyFirstComponent.js
+++ server/generated/MyFirstComponent.js
@@ -1,1 +1,13 @@
-MyFirstComponent
+/** @format */
+
+import React from 'react';
+
+const MyFirstComponent = props => {
+return (
+<div>
+    MyFirstComponent
+</div>
+);
+};
+
+export default MyFirstComponent;
--- server/routes/generate-component-file.js
+++ server/routes/generate-component-file.js
@@ -4,20 +4,23 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const fs = require("fs");
+
+const Handlebars = require("handlebars");
+// var template = Handlebars.compile("Handlebars {{doesWhat}}");
+const compiledTemplate = require("../template/template.js.hbs");
 router.post(
 	"/generateComponentFile",
 	[check("mainComponentName", "mainComponentName is required").not().isEmpty()],
 	(req, res) => {
-		console.log(req.body);
-
 		const errors = validationResult(req);
 		console.log(req.body);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
+
 		fs.writeFile(
 			"./server/generated/" + req.body.mainComponentName + ".js",
-			req.body.mainComponentName,
+			compiledTemplate({ name: req.body.mainComponentName }),
 			function (err) {
 				if (err) {
 					console.error("Failed", err);
@@ -26,7 +29,6 @@ router.post(
 				console.log("Created", err);
 			}
 		);
-		console.log(req.body);
 		res.send("generate component js");
 	}
 );
